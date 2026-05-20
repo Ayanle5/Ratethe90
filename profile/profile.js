8c4cefed6e454fdd44d8bcb99c133305eadae9fa
Ayanle <ayanle3645@gmail.com>
2026-05-20 13:36:58 +0100

    a

--- profile/profile.js
+++ profile/profile.js
@@ -3,3 +3,7 @@ const dmBtn = document.querySelector('.navdm');
 dmBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark');
 });
+
+document.querySelector('.edit-btn').addEventListener('click', () => {
+    document.getElementById('editPopup').style.display = 'block';
+});
